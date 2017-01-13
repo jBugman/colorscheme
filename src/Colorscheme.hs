ef4dcb52ded20d8d9ee7e0648edc473d71d40024
Sergey Parshukov <codedby@bugman.me>
2017-01-13 22:04:41 +0300

    Refactored Palette, first optimisation attempt

--- src/Colorscheme.hs
+++ src/Colorscheme.hs
@@ -1,28 +1,29 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Colorscheme (processImage) where
 
-import qualified Data.ByteString as B
 import Codec.Picture
+import Data.Colour.CIE
+import Data.Colour.SRGB
+import Data.Ord (comparing)
 import Data.Set (Set)
+import Data.Word (Word8)
+import qualified Data.ByteString as B
+import qualified Data.Colour.CIE.Illuminant
+import qualified Data.List as L
 import qualified Data.Set as Set
 import qualified Data.Vector.Storable as V
-import Data.Colour.SRGB
-import Data.Colour.CIE
-import qualified Data.Colour.CIE.Illuminant
-import Data.Word (Word8)
-import qualified Data.List as L
-import Data.Ord (comparing)
-
-type ColorWheelPalette = [PixelRGB8]
+import qualified Palette
 
 processImage :: FilePath -> IO ()
 processImage path = do
   wheel <- loadImage "wheel.png"
-  -- let palette = preparePalette wheel
-  palette <- preparePalette' wheel
+  let palette = Palette.fromImage wheel
+  print palette
   image <- loadImage path
-  -- print $ nearestPaletteColor palette (PixelRGB8 0 42 0)
-  let result = mapDynamicImage palette image
+  let palette' = fmap convertJCtoCC palette
+  print palette'
+  let result = mapDynamicImage' palette' image
+  -- let result = mapDynamicImage palette image
   savePngImage "out.png" result
   putStrLn "done"
 
@@ -31,51 +32,61 @@ loadImage path = do
   eimg <- readImage path
   case eimg of
     Left err -> error ("Could not read image: " ++ err)
-    Right img -> return img
+    Right img -> return . ImageRGB8 $ enforceRGB8 img
 
-preparePalette' :: DynamicImage -> IO ColorWheelPalette
-preparePalette' (ImageRGB8 image@(Image w h _)) = do
-  let mainColors = L.filter (\(_, x) -> (x > 3000) && (x < 30000)) colorsWithCounts
-  let palette = map (head . fst) mainColors ++ [PixelRGB8 0 0 0, PixelRGB8 127 127 127, PixelRGB8 255 255 255] -- adding black, grey, and grey
-  print palette
-  return palette
-  where
-    colorsWithCounts = map (\x->([head x], length x)) . L.group . L.sort $ allPixels
-    allPixels = map (uncurry (pixelAt image)) allCoords :: ColorWheelPalette
-    allCoords = [(x, y) | x <- [0..w-1], y <- [0..h-1]]
-
-preparePalette :: DynamicImage -> ColorWheelPalette
-preparePalette (ImageRGB8 image@(Image w h _)) =
-  Set.toList $ Set.fromList allPixels
-    where
-      allPixels = map (uncurry (pixelAt image)) allCoords
-      allCoords = [(x, y) | x <- [0..w-1], y <- [0..h-1]]
-
-preparePalette _ = error "Unsupported image format"
+-- forces DynamicImage to ImageRGB8
+enforceRGB8 :: DynamicImage -> Image PixelRGB8
+enforceRGB8 (ImageRGB8 image) = image
+enforceRGB8 otherFormat       = convertRGB8 otherFormat
 
 colorDistance :: PixelRGB8 -> PixelRGB8 -> Double
-colorDistance a b = cie76 (convertCCtoJC a) (convertCCtoJC b)
+colorDistance a b = cie76 (convertJCtoCC a) (convertJCtoCC b)
 
-cie76 :: Colour Double -> Colour Double -> Double
+type CColor = Colour Double
+
+cie76 :: CColor -> CColor -> Double
 cie76 c1 c2 = sqrt $ (l2 - l1)^2 + (a2 - a1)^2 + (b2 - b1)^2
   where
     (l1, a1, b1) = toLAB c1
     (l2, a2, b2) = toLAB c2
 
-toLAB :: Colour Double -> (Double, Double, Double)
+toLAB :: CColor -> (Double, Double, Double)
 toLAB = cieLABView Data.Colour.CIE.Illuminant.d65
 
-convertCCtoJC :: PixelRGB8 -> Colour Double
-convertCCtoJC pixel@(PixelRGB8 r g b) = sRGB24 r g b
+convertJCtoCC :: PixelRGB8 -> CColor
+convertJCtoCC pixel@(PixelRGB8 r g b) = sRGB24 r g b
 
-mapDynamicImage :: ColorWheelPalette -> DynamicImage -> DynamicImage
+mapDynamicImage :: Palette.Colors -> DynamicImage -> DynamicImage
 mapDynamicImage palette source = ImageRGB8 $ pixelMap (nearestPaletteColor palette) rgb8image
   where
     rgb8image = case source of
       (ImageRGB8 image) -> image
       otherImage        -> convertRGB8 otherImage
 
-nearestPaletteColor :: ColorWheelPalette -> PixelRGB8 -> PixelRGB8
+nearestPaletteColor :: Palette.Colors -> PixelRGB8 -> PixelRGB8
 nearestPaletteColor palette c = snd $ L.minimumBy (comparing fst) distances
   where
     distances = map (\p -> (colorDistance c p, p)) palette
+
+
+-- Attempt #2. Precalculating palette in CColor
+
+convertCCtoJC :: CColor -> PixelRGB8
+convertCCtoJC c = PixelRGB8 r g b
+  where
+    (RGB r g b) = toSRGB24 c
+
+mapDynamicImage' :: [CColor] -> DynamicImage -> DynamicImage
+mapDynamicImage' palette source = ImageRGB8 $ pixelMap (nearestPaletteColor' palette) rgb8image
+  where
+    rgb8image = case source of
+      (ImageRGB8 image) -> image
+      otherImage        -> convertRGB8 otherImage
+
+nearestPaletteColor' :: [CColor] -> PixelRGB8 -> PixelRGB8
+nearestPaletteColor' palette c = convertCCtoJC . snd $ L.minimumBy (comparing fst) distances
+  where
+    distances = map (\p -> (colorDistance' c p, p)) palette
+
+colorDistance' :: PixelRGB8 -> CColor-> Double
+colorDistance' p c = cie76 (convertJCtoCC p) c
---
+++ src/Palette.hs
@@ -0,0 +1,39 @@
+module Palette (Colors, fromImage) where
+
+import Codec.Picture
+import qualified Data.List as L
+
+
+type Colors = [PixelRGB8]
+
+rgb = PixelRGB8
+
+-- white, gray and black colors 
+monochrome :: Colors
+monochrome = [rgb 0 0 0, rgb 127 127 127, rgb 255 255 255]
+
+extended :: Colors
+extended = monochrome
+  -- green
+  ++ [rgb 194 229 160, rgb 171 218 136, rgb 128 194 65, rgb 73 159 0, rgb 0 116 24, rgb 2 92 27]
+  -- blue-green
+  ++ [rgb 138 229 220, rgb 37 200 174, rgb 4 185 164, rgb 0 135 106, rgb 0 88 94, rgb 0 61 73]
+  -- blue
+  ++ [rgb 169 219 255, rgb 105 189 248, rgb 0 159 232, rgb 0 92 187, rgb 0 63 120, rgb 0 32 88]
+  -- ...
+  -- TODO: finish
+
+fromImage :: DynamicImage -> Colors
+fromImage (ImageRGB8 image@(Image w h _)) = map (head . fst) mainColors ++ monochrome
+  where
+    mainColors = L.filter hardcodedFrequencies colorsWithCounts
+    hardcodedFrequencies (_, x) = (x > 3000) && (x < 30000) -- for wheel.png
+    colorsWithCounts = countItems allPixels
+    allPixels = map (uncurry (pixelAt image)) allCoords
+    allCoords = [(x, y) | x <- [0..w-1], y <- [0..h-1]]
+
+
+countItems :: (Eq a, Ord a) => [a] -> [([a], Int)]
+countItems = fmap counter . L.group . L.sort
+  where
+    counter x = ([head x], length x)
